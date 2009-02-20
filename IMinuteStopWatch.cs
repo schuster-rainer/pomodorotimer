2f0098d24363c13bcf331ccdb95ec2d220136feb
schuster-rainer@web.de <schuster-rainer@web.de@886305a2-fcfe-11dd-ada4-5153b8187bf2>
2009-02-20 17:11:30 +0000

    git-svn-id: https://pomodorotimer.googlecode.com/svn/trunk@5 886305a2-fcfe-11dd-ada4-5153b8187bf2

---
+++ IMinuteStopWatch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace PomodoroTimer
+{
+	public interface IMinuteStopWatch
+	{
+		void Start ();
+		void Stop ();
+		int Countdown { get; set; }
+		
+		event EventHandler Alert;
+		event EventHandler<CountDownEventArgs> Tick;
+	}
+}
